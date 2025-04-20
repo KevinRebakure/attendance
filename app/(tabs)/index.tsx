e0de40c213efefd96922c423c015eccf5e01a8c1
Rebakure <kevinrebakure@gmail.com>
2025-04-20 15:27:22 +0200

    feat: Create groups screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { Colors } from "@/constants/colors";
+import { Text, TouchableHighlight, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function Index() {
   return (
-    <View className="flex items-center justify-center h-full">
-      <Text>People tab</Text>
+    <View className="m-3 flex gap-4">
+      {/* Top navigation */}
+      <View className="flex flex-row justify-between items-center">
+        <View className="flex flex-row gap-2">
+          <Text className="font-semibold">Host</Text>
+          <Text className="text-secondaryText">Guest</Text>
+        </View>
+        <Ionicons name="notifications-outline" size={24} color="black" />
+      </View>
+
+      {/* Create a new group */}
+      <TouchableHighlight
+        onPress={() => console.log("Create new group")}
+        className="bg-primary rounded-lg self-start px-4 py-3 mt-4 active:text-primary"
+        underlayColor={Colors.darkPrimary}
+      >
+        <Text className="text-white">+ Create a new group</Text>
+      </TouchableHighlight>
     </View>
   );
 }
