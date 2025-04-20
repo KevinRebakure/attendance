f4726a4d12d9a1fdaa8f7268b83af2a75c955f64
Rebakure <kevinrebakure@gmail.com>
2025-04-20 15:26:51 +0200

    Customize tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,18 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3F51B5",
+        darkPrimary: "#303F9F",
+        lightPrimary: "#C5CAE9",
+        accent: "#4CAF50",
+        primaryText: "#212121",
+        secondaryText: "#757575",
+        dividor: "#BDBDBD",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
