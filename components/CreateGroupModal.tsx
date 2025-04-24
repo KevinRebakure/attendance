a5762a8a5e292eabf4c355687aa233ee1e8ed5d4
Rebakure <kevinrebakure@gmail.com>
2025-04-24 23:35:38 +0200

    refactor: add more dummy groups

--- components/CreateGroupModal.tsx
+++ components/CreateGroupModal.tsx
@@ -13,7 +13,7 @@ export default function CreateGroupModal({
   const [input, setInput] = useState("");
   return (
     <Modal animationType="slide" transparent={true} visible={isOpen}>
-      <View className="flex-1 justify-center items-center">
+      <View className="flex-1 justify-center items-center bg-black/50">
         <View className="w-5/6 bg-white rounded-2xl p-5 shadow-md flex gap-4">
           <Text className="text-2xl font-bold">Create a new group</Text>
           <Text>Group name</Text>
