05461a854e453be6700b8f500c40c932d691213e
Rebakure <kevinrebakure@gmail.com>
2025-04-24 23:07:57 +0200

    feat: add a flashlist of group items

--- app/(tabs)/(groups)/index.tsx
+++ app/(tabs)/(groups)/index.tsx
@@ -1,14 +1,41 @@
+import { GroupListItem } from "@/components";
 import CreateGroupModal from "@/components/CreateGroupModal";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+
+const initialGroups = [
+  {
+    name: "ALU",
+    id: "1",
+  },
+  {
+    name: "TheGym",
+    id: "2",
+  },
+  {
+    name: "End of year party",
+    id: "3",
+  },
+];
 
 export default function Host() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [groups, setGroups] = useState(initialGroups);
+
   return (
     <>
       <View className="flex items-center justify-center h-full">
-        <Text>No groups created yet!</Text>
+        {groups.length === 0 && <Text>No groups created yet!</Text>}
+
+        <FlatList
+          data={groups}
+          renderItem={({ item }) => (
+            <GroupListItem name={item.name} key={item.id} />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+
         <CreateGroupModal
           isOpen={modalVisible}
           closeModal={() => setModalVisible(false)}
