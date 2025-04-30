3999f3ace27c5ed9fd67eb7df160722e9b7099eb
Rebakure <kevinrebakure@gmail.com>
2025-04-30 20:16:10 +0200

    feat: userProfileStore

---
+++ stores/userProfileStore.ts
@@ -0,0 +1,14 @@
+// import { create } from "zustand";
+
+// const useUserProfileStore = create((set) => ({
+//   userProfile: {
+//     username: "Kevin Rebakure",
+//     email: "kevinrebakure@gmail.com",
+//     currentPassword: "abcd@1234E",
+//     newPassword: "",
+//     repeatPassword: "",
+//   },
+//   editUserName: () => set(() => {}),
+//   editEmail: () => set(() => {}),
+//   editEmail: () => set(() => {}),
+// }));
