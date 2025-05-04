226752ee8cc87896d01da9f8a950246d9d13b077
Rebakure <kevinrebakure@gmail.com>
2025-05-04 13:29:16 +0200

    Update camera.tsx

--- app/(tabs)/(qrcode)/camera.tsx
+++ app/(tabs)/(qrcode)/camera.tsx
@@ -32,39 +32,35 @@ export default function Camera() {
   }
 
   return (
-    <>
-      <CameraView
-        barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
-        onBarcodeScanned={handleBarCodeScanned}
-      >
-        <View className="h-full p-6">
-          <View className="flex items-center h-full justify-evenly">
-            <Text className="text-2xl font-bold text-primary">
-              Scan QR code
-            </Text>
-            <Image
-              source={require("@/assets/images/qr-scan.png")}
-              className="w-60 h-60"
-            />
+    <CameraView
+      barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
+      onBarcodeScanned={handleBarCodeScanned}
+    >
+      <View className="h-full p-6">
+        <View className="flex items-center h-full justify-evenly">
+          <Text className="text-2xl font-bold text-primary">Scan QR code</Text>
+          <Image
+            source={require("@/assets/images/qr-scan.png")}
+            className="w-60 h-60"
+          />
 
-            <View className="flex flex-row gap-8 bg-primary p-4 rounded-full">
-              <Pressable onPress={scanFromImage}>
-                <Ionicons name="images-outline" size={28} color="white" />
-              </Pressable>
-              <Pressable onPress={turnOnFlashlight}>
-                <MaterialCommunityIcons
-                  name="lightning-bolt-outline"
-                  size={28}
-                  color="white"
-                />
-              </Pressable>
-            </View>
+          <View className="flex flex-row gap-8 bg-primary p-4 rounded-full">
+            <Pressable onPress={scanFromImage}>
+              <Ionicons name="images-outline" size={28} color="white" />
+            </Pressable>
+            <Pressable onPress={turnOnFlashlight}>
+              <MaterialCommunityIcons
+                name="lightning-bolt-outline"
+                size={28}
+                color="white"
+              />
+            </Pressable>
           </View>
-          <Pressable onPress={closeCamera} className="absolute top-4 left-4">
-            <Ionicons name="close" size={36} color="black" />
-          </Pressable>
         </View>
-      </CameraView>
-    </>
+        <Pressable onPress={closeCamera} className="absolute top-4 left-4">
+          <Ionicons name="close" size={36} color="black" />
+        </Pressable>
+      </View>
+    </CameraView>
   );
 }
