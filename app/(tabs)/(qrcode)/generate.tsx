68a08e0ddf7e1e069d979662b4b74e76d9546c93
Rebakure <kevinrebakure@gmail.com>
2025-05-05 21:03:56 +0200

    feat: generate qr code

--- app/(tabs)/(qrcode)/generate.tsx
+++ app/(tabs)/(qrcode)/generate.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import QRCode from "react-qr-code";
+import { qrCodes } from ".";
 
 export default function Generate() {
+  const event = qrCodes[0];
   return (
     <View>
-      <Text>Generate</Text>
+      <QRCode value={JSON.stringify(event)} size={256} />
     </View>
   );
 }
